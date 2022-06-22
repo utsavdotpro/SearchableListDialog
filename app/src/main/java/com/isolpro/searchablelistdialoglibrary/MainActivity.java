497a5df815f95c867ed5efc771d2955606a37161
Utsav Barnwal <u.barnwal36@gmail.com>
2022-06-22 23:34:22 +0530

    open dialog on button click

--- app/src/main/java/com/isolpro/searchablelistdialoglibrary/MainActivity.java
+++ app/src/main/java/com/isolpro/searchablelistdialoglibrary/MainActivity.java
@@ -1,37 +1,44 @@
 package com.isolpro.searchablelistdialoglibrary;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.isolpro.library.searchablelistdialog.SearchableListDialog;
+import com.isolpro.searchablelistdialoglibrary.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+  private ActivityMainBinding binding;
+  private SearchableListDialog<String> dialogSearchableList;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
 
-    SearchableListDialog<String> dialogSearchableList = new SearchableListDialog<>(this, new SearchableListAdapter(this));
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
 
+    // Open dialog on button click
+    binding.bChooseItem.setOnClickListener(v -> dialogSearchableList.show());
+
+    // Initiate SearchListDialog
+    dialogSearchableList = new SearchableListDialog<>(this, new SearchableListAdapter(this));
     dialogSearchableList.setSearchMatcher((item, keyword) -> item.toLowerCase().contains(keyword));
 
-    dialogSearchableList.setOnItemSelectedListener(item ->
-      Toast.makeText(this, item + " selected!", Toast.LENGTH_SHORT).show()
-    );
+    // Callback on item selection
+    dialogSearchableList.setOnItemSelectedListener(item -> binding.bChooseItem.setText(item));
+
+    // Callback on nothing selected
+    dialogSearchableList.setOnNothingSelectedListener(() -> binding.bChooseItem.setText("Choose Item"));
 
     List<String> items = new ArrayList<>();
-
     items.add("A");
     items.add("B");
 
     dialogSearchableList.setItems(items);
-
-    dialogSearchableList.show();
   }
 }
